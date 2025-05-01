f0076fa97d81e8e4ceaa612eb31f78ec2306d8e0
Matheus Brandão Nogueura <161345684+mathiaspy22@users.noreply.github.com>
2025-05-01 19:33:02 -0300

    Add files via upload
    Adicionando mais de uma pessoa em um banco de dados.

---
+++ inserindo dados no meu banco.sql
@@ -0,0 +1,9 @@
+insert into pessoas values
+(default,'Pedro', '1999-04-20','M','80.2','1.90',default),
+(default,'Godofredo','1890-10-01','M','76.4','1.59','Canadá'),
+(default,'Luiza', '2007-05-22','F', '56.2','1.55', default);
+
+select * from pessoas
+
+
+
